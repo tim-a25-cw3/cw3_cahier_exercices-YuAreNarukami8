d640b2c213717c1efc2d9e1e02667480f78b34d6
YuAreNarukami8 <yanmahive@gmail.com>
2025-11-12 10:55:15 -0500

    Sauvegarde

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,5 +1,5 @@
 import Icons from './utils/Icons.js';
-
+import Swiper from 'swiper/bundle';
 class Main {
   constructor() {
     this.init();
@@ -7,6 +7,19 @@ class Main {
 
   init() {
     Icons.load();
+    this.initSwiperPagination();
+  }
+  initSwiperPagination() {
+    const target = document.querySelector('.js-swiper-labo');
+    if (target) {
+      const swiper = new Swiper(target, {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+          clickable: true,
+        },
+      });
+    }
   }
 }
 new Main();
